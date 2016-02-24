6fdd511e50e8e055dfceafbc3463278f6dcda9ac
Stanko <stanko.trcek@gmail.com>
2016-02-24 12:37:48 +0100

    Add 001 create and loop matrix

---
+++ matrix/001.create.and.loop.matrix.R
@@ -0,0 +1,13 @@
+# create matrix 5 row by 2 columns
+per <- matrix(c("2008","2009",
+                "2009","2010",
+                "2010","2011",
+                "2011","2012",
+                "2012","2013"), nrow = 5, byrow = TRUE)
+per
+
+for (i in 1:dim(per)[1])
+{
+  print(paste0( "row: ",i, " columns: ",per[i,1],", ",per[i,2]), quote = FALSE)
+}
+

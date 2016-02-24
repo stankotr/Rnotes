60a62f53fcc8d16d471d9e6df4e69e875548e24b
Stanko <stanko.trcek@gmail.com>
2016-02-24 12:39:32 +0100

    Update 001.create.and.loop.matrix.R

--- matrix/001.create.and.loop.matrix.R
+++ matrix/001.create.and.loop.matrix.R
@@ -4,8 +4,10 @@ per <- matrix(c("2008","2009",
                 "2010","2011",
                 "2011","2012",
                 "2012","2013"), nrow = 5, byrow = TRUE)
+# show matrix
 per
 
+# loop matrix and print row value
 for (i in 1:dim(per)[1])
 {
   print(paste0( "row: ",i, " columns: ",per[i,1],", ",per[i,2]), quote = FALSE)
